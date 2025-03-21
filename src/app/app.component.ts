385413d70c0258275fb96c1f53b50bfa0ec7f4a8
Yahia <yahiaaljanabi@gmail.com>
2025-03-21 02:08:52 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Portfolio_v5';
-}
+export class AppComponent {}
